f82b12035b5a38403a2cba4b61b458c96c4392b5
HypoxiE <kosmaer42@gmail.com>
2026-06-27 21:06:08 +0300

    fixed backups

--- src/database/settings/db_settings.py
+++ src/database/settings/db_settings.py
@@ -2,6 +2,6 @@ import os
 
 DB_HOST=os.getenv("DB_HOST", "localhost")
 DB_PORT=os.getenv("DB_PORT", "5432")
-DB_USER=os.getenv("DB_USER", "discord_economy_bot")
-DB_PASS=os.getenv("DB_PASSWORD", "economy_bot")
-DB_NAME=os.getenv("DB_NAME", "discord_economy_bot_db")
+DB_USER=os.getenv("DB_USER", "discord_moderation_bot")
+DB_PASS=os.getenv("DB_PASSWORD", "moderation_bot")
+DB_NAME=os.getenv("DB_NAME", "discord_moderation_bot_db")
